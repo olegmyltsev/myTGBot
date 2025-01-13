140423202cf8cb5e4c80144094fc9149a4e435d2
olegmyltsev <oleg.myltsev.2008@gmail.com>
2025-01-13 15:36:48 +0300

    create tg bot in bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,25 @@
+import telebot
+from telebot import types
 
-print('6')
+token: str = '7896898992:AAHeeBulSBtvseJIkdJ3mU3-vpHvT0k6isE'
+bot = telebot.TeleBot(token)
+isWelcomeSends: bool = False
 
-print('vasdvadvasd')
-print('dev 3')
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    itembtn = types.KeyboardButton('Press me!')
+    markup.add(itembtn)
+
+    msg = bot.send_message(message.chat.id, "Hello, press the button!", reply_markup=markup)
+
+    bot.register_next_step_handler(msg, test)
+
+def test(message):
+    print(message.text)
+    bot.send_message(message.chat.id,'You send me message', reply_markup=types.ReplyKeyboardRemove())
 
 
+bot.infinity_polling()
